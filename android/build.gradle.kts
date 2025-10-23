98473e75f849b0e5abefabcad7725f3bf9eb80ef
Maurice Tamman <maurice@tamman.org>
2025-10-23 06:58:38 -0400

    Set Java version to 11 for all projects and suppress Java version warnings

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,20 @@ allprojects {
     }
 }
 
+// Set Java version for all projects
+subprojects {
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "11"
+        }
+    }
+
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = JavaVersion.VERSION_11.toString()
+        targetCompatibility = JavaVersion.VERSION_11.toString()
+    }
+}
+
 val newBuildDir: Directory =
     rootProject.layout.buildDirectory
         .dir("../../build")
